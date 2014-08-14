430a4006a4d5cbaed94e03a6a5079b1fec8957ec
Vladimir Still <xstill@fi.muni.cz>
2014-08-14 10:18:11 +0200

    testing: Add missing file.

---
+++ testing/jonas_expression_testing/Types/Formating.hs
@@ -0,0 +1,34 @@
+module Types.Formating ( FormatType ( formatType ), formatContext ) where
+
+import Types.TypeExpression
+import Data.List ( intercalate )
+
+class FormatType t where
+    formatType :: t -> String
+
+instance FormatType Type where
+    formatType (TypeApplication t1 t2) = _parens (formatType t1) ++ " " ++ _parens (formatType t2)
+    formatType (TypeConstructor con)   = con
+    formatType (FunctionType t1 t2) = (\x -> x ++ " -> " ++ formatType t2) (case t1 of
+                                          FunctionType _ _ -> _parens (formatType t1)
+                                          _                -> formatType t1)
+    formatType (VariableType tvar) = tvar
+    formatType (TupleType tts)     = _tuple (map formatType tts)
+    formatType (ListType lt)       = '[' : formatType lt ++ "]"
+
+instance FormatType TypeExpression where
+    formatType (TypeExpression (TypeContext []) ty) = formatType ty
+    formatType (TypeExpression con ty) = formatContext con ++ " => " ++ formatType ty
+      where
+
+
+formatContext :: TypeContext -> String
+formatContext (TypeContext []) = "()"
+formatContext (TypeContext [(c, v)]) = c ++ " " ++ v
+formatContext (TypeContext cs) = _tuple (map (\(c, v) -> c ++ " " ++ v) cs)
+
+_parens :: String -> String
+_parens x = if ' ' `elem` x then '(' : x ++ ")" else x
+
+_tuple :: [String] -> String
+_tuple x = '(' : intercalate ", " x ++ ")"
