f5463a931cf44c37dd45512a56da7dc2c81605e3
Vladimir Still <xstill@fi.muni.cz>
2014-08-19 18:22:02 +0200

    testing: Add generalized curry/uncurry functions.

---
+++ testing/jonas_expression_testing/src/Types/Curry.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE TypeFamilies #-}
+-- | Generic curry for 2 to 7 tuples
+module Types.Curry ( GCurry( Curry, guncurry, gcurry ) ) where
+
+class GCurry a where
+    type Curry p r :: *
+    gcurry   :: (a -> r) -> Curry a r
+    guncurry :: Curry a r -> a -> r
+
+instance GCurry (a, b) where
+    type Curry (a, b) r = a -> b -> r
+    gcurry = curry
+    guncurry = uncurry
+
+instance GCurry (a, b, c) where
+    type Curry (a, b, c) r = a -> b -> c -> r
+    gcurry fun a b c = fun (a, b, c)
+    guncurry fun (a, b, c) = fun a b c
+
+instance GCurry (a, b, c, d) where
+    type Curry (a, b, c, d) r = a -> b -> c -> d -> r
+    gcurry fun a b c d = fun (a, b, c, d)
+    guncurry fun (a, b, c, d) = fun a b c d
+
+instance GCurry (a, b, c, d, e) where
+    type Curry (a, b, c, d, e) r = a -> b -> c -> d -> e -> r
+    gcurry fun a b c d e = fun (a, b, c, d, e)
+    guncurry fun (a, b, c, d, e) = fun a b c d e
+
+instance GCurry (a, b, c, d, e, f) where
+    type Curry (a, b, c, d, e, f) r = a -> b -> c -> d -> e -> f -> r
+    gcurry fun a b c d e f = fun (a, b, c, d, e, f)
+    guncurry fun (a, b, c, d, e, f) = fun a b c d e f
+
+instance GCurry (a, b, c, d, e, f, g) where
+    type Curry (a, b, c, d, e, f, g) r = a -> b -> c -> d -> e -> f -> g -> r
+    gcurry fun a b c d e f g = fun (a, b, c, d, e, f, g)
+    guncurry fun (a, b, c, d, e, f, g) = fun a b c d e f g
+
