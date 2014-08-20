8607b2778efce0eca6fed8bd0e19cc76fba519de
Vladimir Still <vl.still@gmail.com>
2014-08-20 18:25:14 +0200

    test: Add security tests.

---
+++ tests/Security.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE TupleSections #-}
+module Main ( main ) where
+
+import Types.Curry
+import Harness
+
+main = runTests . flip (zipWith guncurry) (repeat ("f = map", "f", WontCompile ignored)) $
+    [ ("import qualified Solution\nf = Solution.f",,,)
+    , ("import System.IO.Unsafe\nf = unsafePerformIO (return map)",,,)
+    -- implicit import
+    , ("f = Solution.f",,,)
+    , ("f = System.IO.Unsafe.unsafePerformIO (returm map)",,,)
+    ]
