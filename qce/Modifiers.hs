fb67242b3090b9e8e727e033abe45cfdeed198e8
Vladimir Still <vl.still@gmail.com>
2014-09-18 21:38:12 +0200

    qce: Generalize to union of ranges (Ranges modifier).
    Range is now defined as type alias for Ranges.

--- qce/Modifiers.hs
+++ qce/Modifiers.hs
@@ -1,13 +1,19 @@
 {-# LANGUAGE DataKinds
            , KindSignatures
+           , PolyKinds
+           , TypeOperators
            , GADTs
            , ExplicitForAll
            , ScopedTypeVariables
            #-}
 
+-- (c) 2014 Vladimír Štill
+
 module Modifiers
-    ( Range ( Range ), unRange )
-    where
+    ( Range, Ranges ( Range ), unRange
+    , CRange ( toRanges )
+    , inRanges
+    ) where
 
 import GHC.TypeLits
 import Data.Proxy
@@ -18,21 +24,39 @@ import Test.QuickCheck
 import Test.QuickCheck.Arbitrary
 
 -- | modifier for generating integral values within given inclusive range
-data Range :: * -> Nat -> Nat -> * where
-    Range :: (Integral i, Arbitrary i) => { unRange  :: i } -> Range i from to
+type Range (i :: *) (from :: Nat) (to :: Nat) = Ranges i '[ '(from, to) ]
 
-instance Show i => Show (Range i from to) where
+data Ranges :: * -> [ (Nat, Nat) ] -> * where
+    Range :: (Integral i, Arbitrary i) => { unRange :: i } -> Ranges i ranges
+
+instance Show i => Show (Ranges i ranges) where
     show = unRange >>> show
 
-instance forall i from to.
-         (Integral i, Arbitrary i, Random i, KnownNat from, KnownNat to)
-         => Arbitrary (Range i (from :: Nat) (to :: Nat))
+class CRange (a :: k) where
+    toRanges :: Proxy a -> [(Integer, Integer)]
+
+-- note that the tuple here in instance header is data constuctor lifted to type
+instance forall from to. (KnownNat from, KnownNat to) => CRange '(from, to) where
+    toRanges _ = [ (natVal (Proxy :: Proxy from), natVal (Proxy :: Proxy to)) ]
+
+instance forall head tail. (CRange head, CRange tail) => CRange (head ': tail) where
+    toRanges _ = toRanges (Proxy :: Proxy head) ++ toRanges (Proxy :: Proxy tail)
+
+instance CRange '[] where
+    toRanges _ = []
+
+inRanges :: Integral i => i -> [(Integer, Integer)] -> Bool
+inRanges val0 = any (\(x, y) -> val >= x && val <= y)
+  where val = fromIntegral val0
+
+instance forall i ranges.
+         (Integral i, Arbitrary i, Random i, CRange ranges)
+         => Arbitrary (Ranges i ranges)
     where
-        arbitrary = fmap Range $ choose (fromIntegral (natVal (Proxy :: Proxy from))
-                                        , fromIntegral (natVal (Proxy :: Proxy to)))
+        arbitrary = map (fromIntegral *** fromIntegral >>> choose) >>>
+                    oneof >>> fmap Range $ toRanges (Proxy :: Proxy ranges)
+
         shrink = unRange >>>
                  shrinkIntegral >>>
-                 filter (>= fromIntegral (natVal (Proxy :: Proxy from))) >>>
-                 filter (<= fromIntegral (natVal (Proxy :: Proxy to))) >>>
-                 map (Range :: i -> Range i from to)
-
+                 filter (flip inRanges (toRanges (Proxy :: Proxy ranges))) >>>
+                 map Range
