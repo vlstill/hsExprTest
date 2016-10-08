8b225ba264d59a797d9ddf90f3d758bb57b67a71
Vladimir Still <xstill@fi.muni.cz>
2016-10-08 14:19:27 +0200

    Fix wrapper, add simple tester script.

---
+++ testService.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+QDIR=$HOME/repo/ib015_questions
+
+[[ -n $1 ]] && QDIR=$1
+
+echo "QDIR = $QDIR" >&2
+
+if ! echo $TERM | grep -iq screen; then
+    echo "This test must run in screen to execute multiple terminals" >&2
+    exit
+fi
+
+echo | socat - UNIX-CONNECT:sock >&/dev/null || screen ./hsExprTestService sock $QDIR
+
+echo "enter question id:"
+read id
+
+echo "enter the question:"
+data=""
+while read line; do
+    data="$data$line\n"
+done
+
+echo -en "I0Q${id}S${data}" | socat - UNIX-CLIENT:sock
