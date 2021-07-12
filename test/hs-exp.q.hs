05fdf0ff2b4e66e1d84581460c60953d4dfbfec2
Vladimir Still <git@vstill.eu>
2021-07-12 14:28:58 +0200

    test: Quotes have to be typed explicitly now

--- test/hs-exp.q.hs
+++ test/hs-exp.q.hs
@@ -1,9 +1,11 @@
 -- @ compiled: True
 
 import Test.QuickCheck
+import Language.Haskell.TH ( Q, Pat )
 
 expr = "myexp"
 timeout = 10
+pattern :: Q Pat
 pattern = [p| (NonNegative x, NonNegative y) |]
 
 myexp :: Integer -> Integer -> Integer
--- test/hs-pat-untyped.q.hs
+++ test/hs-pat-untyped.q.hs
@@ -1,6 +1,8 @@
 import Test.QuickCheck.Modifiers ( NonNegative ( NonNegative ) )
+import Language.Haskell.TH ( Q, Pat )
 
 expr = "f"
+pattern :: Q Pat
 pattern = [p| (NonNegative x) |]
 
 f :: Int -> Int
