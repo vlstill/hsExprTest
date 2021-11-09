ee9c4cf65c3a462620e3a81034c9275285387360
Vladimir Still <git@vstill.eu>
2021-11-09 20:23:51 +0100

    frag: Implement support for reflection of student tasks

---
+++ frag/common/mkreflect.pl
@@ -0,0 +1,37 @@
+use warnings;
+use strict;
+use v5.16;
+use open qw(:encoding(UTF-8) :std);
+
+say "{-# LANGUAGE TemplateHaskell, Unsafe #-}";
+say "module StudentReflector ( studentAST ) where";
+say "";
+
+my $header = 1;
+my $import = 0;
+while ( <> ) {
+    if ( /^\s*$/ ) {
+        print;
+    }
+    elsif ( not $header ) {
+        print "    $_";
+    }
+    elsif ( /^import/ ) {
+        $import = 1;
+        print;
+    }
+    elsif ( $import && /^\s/ ) {
+        print;
+    }
+    else {
+        $header = 0;
+        $import = 0;
+        say "";
+        say "import Language.Haskell.TH ( Q, Dec )";
+        say "";
+        say "studentAST :: Q [Dec]";
+        say "studentAST = [d|";
+        print "    $_";
+    }
+}
+say "  |]"
